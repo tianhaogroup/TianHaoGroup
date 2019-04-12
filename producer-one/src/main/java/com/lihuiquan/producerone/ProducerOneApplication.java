110aa020b7d8be057c8095b97bc4b483f67bbb0a
lyq <466703629@qq.com>
2019-04-12 15:20:22 +0800

    Initial Commit

---
+++ producer-one/src/main/java/com/lihuiquan/producerone/ProducerOneApplication.java
@@ -0,0 +1,13 @@
+package com.lihuiquan.producerone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerOneApplication.class, args);
+    }
+
+}
